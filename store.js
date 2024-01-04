d84ae27f586629185ebb0f3e96e517032fa52269
balldev1 <balldev1@hotmail.com>
2024-01-04 21:43:10 +0700

    first commit

---
+++ store.js
@@ -0,0 +1,12 @@
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware'
+
+
+export const useStore = create(
+    devtools((set) => ({
+
+        bears: 0,
+        increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+        test: () => set((state) => ({ bears: state.bears - 1 })),
+        removeAllBears: () => set({ bears: 0 }),
+    })));
